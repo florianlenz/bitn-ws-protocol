ba5a95f4d53e151b963272e80f597ec4589d504b
Florian Lenz <flenz81@googlemail.com>
2017-09-19 10:51:21 +0200

    Worked on websocket implementation user search

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY ./ /app
 WORKDIR /app
 
 RUN npm install
+RUN npm run build
 
 CMD ["node", "index.js"]
 
--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require('express')();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const levelup = require('levelup');
 
-app.listen(80);
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
 
-function handler (req, res) {
-    res.writeHead(200);
-    res.end("<h1>Websocket - Running</h1>");
-}
+http.listen(80);
+
+const clients = [];
 
 io.on('connection', function (socket) {
 
-    console.log(socket.handshake.query['pubKey']);
+    /**
+     * Set clients
+     */
+    clients.push({
+        socketId: socket.id,
+        pubKey: socket.handshake.query['pubKey']
+    });
+
+    console.log(clients);
 
     //get instructions to search user
     socket.on('search:user', function (data) {
@@ -48,5 +58,37 @@ io.on('connection', function (socket) {
         //1. ?Check if user is connected to this node / true -> send message / false -> send to another node
 
     });
+
+    //Logic to share nodes
+    socket.on('nodes:share', function(data){
+        data = JSON.parse(data);
+
+        //Node can contain address port
+        const nodes = data.nodes;
+
+        //1. Add to list of nodes
+        //2. fetch list of nodes and pass back
+
+        const savedNodes = [];
+
+        socket.emit('nodes:share', JSON.stringify(savedNodes));
+
+    });
+
+    socket.on('disconnect', function(socket){
+
+        for(var i = 0; i < clients.length; i++){
+
+            const client = clients[i];
+
+            if(client.socketId === socket.id){
+                if (i > -1) {
+                    clients.splice(i, 1);
+                }
+            }
+
+        }
+
+    })
 
 });
